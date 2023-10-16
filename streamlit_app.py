b24d365407cbd61fe0c5792b6ce18daab5fbaaab
Saewyn <148080728+Saewyn@users.noreply.github.com>
2023-10-16 16:49:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd 
 my_fruit_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
+my_fruit_list= my_fruit_list.set_index('Fruit')
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -11,5 +11,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
