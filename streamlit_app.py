91e95f752775378bd8c974680583f05489638c87
Saewyn <148080728+Saewyn@users.noreply.github.com>
2023-10-16 16:50:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
+# Parameters and Imports
 import streamlit
 import pandas as pd 
 my_fruit_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list= my_fruit_list.set_index('Fruit')
 
+# Here starts the menu
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header("Breakfast Fav's")
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
