b137d35869bc0ebb2139a970cc4edf49e1939c4f
Saewyn <148080728+Saewyn@users.noreply.github.com>
2023-10-16 16:52:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build Your Own Fruit Smoothie')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries')
 streamlit.dataframe(my_fruit_list)
